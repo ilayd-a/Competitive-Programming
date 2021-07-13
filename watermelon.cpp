d8442f4de190d80d4d9ac18346c23aa0a58d0fd8
ilayd-a <66847423+ilayd-a@users.noreply.github.com>
2021-07-13 23:14:05 +0300

    Create watermelon.cpp

---
+++ watermelon.cpp
@@ -0,0 +1,10 @@
+//https://codeforces.com/problemset/problem/4/A
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    if(n % 2 == 1 || n == 2) cout << "NO" << endl;
+    else cout << "YES" << endl;
+}
+
