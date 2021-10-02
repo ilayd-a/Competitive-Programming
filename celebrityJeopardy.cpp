01ea865eebcf6de3a71b5559a609bf5389dfa369
ilayd-a <66847423+ilayd-a@users.noreply.github.com>
2021-10-02 17:52:05 +0300

    Create celebrityJeopardy.cpp

---
+++ celebrityJeopardy.cpp
@@ -0,0 +1,10 @@
+//https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3565
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main() {
+    string str;
+    while(getline(cin,str)){
+        cout << str << endl;
+    }
+}
