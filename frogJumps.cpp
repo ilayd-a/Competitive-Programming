9d802b469cca686a2d25244e997648f1e248fc53
ilayd-a <66847423+ilayd-a@users.noreply.github.com>
2022-01-01 16:13:33 +0300

    Create frogJumps.cpp

---
+++ frogJumps.cpp
@@ -0,0 +1,24 @@
+//https://codeforces.com/problemset/problem/1324/C
+#include <iostream>
+#include <vector>
+#include <cstring>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        vector <int>pos;
+        pos.push_back(0);
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='R') pos.push_back(i+1);
+        }
+        pos.push_back(s.size()+1);
+        int ans=0;
+        for(int i=0;i<pos.size()-1;i++){
+            ans = max(ans, pos[i+1]-pos[i]);
+        }
+        cout << ans << endl;
+    }
+}
